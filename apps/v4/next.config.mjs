2a7744fd34495af6a3be2389214ed956fbb187ed
Cong-Cong <dacongsama@live.com>
2025-07-30 10:31:58 +0800

    perf: using NextExternalsPlugin for external dependencies

--- apps/v4/next.config.mjs
+++ apps/v4/next.config.mjs
@@ -1,5 +1,6 @@
 import { createMDX } from "fumadocs-mdx/next"
 import withRspack from "next-rspack"
+import mod from "module";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -71,7 +72,7 @@ const nextConfig = {
       },
     ]
   },
-  webpack(webpackConfig, { webpack, config, dir }) {
+  webpack(webpackConfig, { webpack, config, dir, isServer }) {
     if (process.env.RSPACK_TRACE) {
       if (!globalThis.registerGlobalTrace) {
         webpack.experiments.globalTrace.register(
@@ -84,11 +85,24 @@ const nextConfig = {
     }
 
     if ("NextExternalsPlugin" in webpack) {
-      delete webpackConfig.externals
+      if (!isServer) {
+        console.log("\nUsing NextExternalsPlugin for external dependencies\n");
+      }
+      webpackConfig.externals = [];
 
       const compilerType = webpackConfig.name
       // eslint-disable-next-line @typescript-eslint/no-require-imports
-      const builtinModules = require('module').builtinModules
+      const builtinModules = mod.builtinModules
+      const optOutBundlingPackageRegex = /[/\\]node_modules[/\\](@appsignal[/\\]nodejs|@aws-sdk[/\\]client-s3|@aws-sdk[/\\]s3-presigned-post|@blockfrost[/\\]blockfrost-js|@highlight-run[/\\]node|@huggingface[/\\]transformers|@jpg-store[/\\]lucid-cardano|@libsql[/\\]client|@mikro-orm[/\\]core|@mikro-orm[/\\]knex|@node-rs[/\\]argon2|@node-rs[/\\]bcrypt|@prisma[/\\]client|@react-pdf[/\\]renderer|@sentry[/\\]profiling-node|@sparticuz[/\\]chromium|@sparticuz[/\\]chromium-min|@swc[/\\]core|@xenova[/\\]transformers|argon2|autoprefixer|aws-crt|bcrypt|better-sqlite3|canvas|chromadb-default-embed|config|cpu-features|cypress|dd-trace|eslint|express|firebase-admin|htmlrewriter|import-in-the-middle|isolated-vm|jest|jsdom|keyv|libsql|mdx-bundler|mongodb|mongoose|newrelic|next-mdx-remote|next-seo|node-cron|node-pty|node-web-audio-api|onnxruntime-node|oslo|pg|playwright|playwright-core|postcss|prettier|prisma|puppeteer|puppeteer-core|ravendb|require-in-the-middle|rimraf|sharp|shiki|sqlite3|ts-node|ts-morph|typescript|vscode-oniguruma|webpack|websocket|zeromq)[/\\]/
+      const finalTranspilePackages = [
+        'geist',
+        ...config.transpilePackages ?? [],
+      ]
+      for (const pkg of config.experimental.optimizePackageImports || []) {
+        if (!finalTranspilePackages.includes(pkg)) {
+          finalTranspilePackages.push(pkg)
+        }
+      }
       webpackConfig.plugins.push(
         new webpack.NextExternalsPlugin({
           compilerType,
@@ -97,7 +111,7 @@ const nextConfig = {
           optOutBundlingPackageRegex,
           finalTranspilePackages,
           dir,
-          defaultOverrides
+          defaultOverrides: {}
         })
       )
     }
